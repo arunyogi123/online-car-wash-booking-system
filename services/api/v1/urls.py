78c0d48132523d2a29849070f444f643237d5702
Arun Yogi <yogiarun290@gmail.com>
2026-07-13 20:06:15 +0545

    feat:working on service updata-delete

--- services/api/v1/urls.py
+++ services/api/v1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ServiceView
+from .views import ServiceView,ServiceManage
 
 urlpatterns=[
-    path("service/",ServiceView.as_view(),name="get-post-service")
+    path("service/",ServiceView.as_view(),name="get-post-service"),
+    path("manage/<int:id>/",ServiceManage.as_view(),name="update-manage")
 ]
