3c56288fda00c2f1c7348015686481cada5434d2
Arun Yogi <yogiarun290@gmail.com>
2026-07-13 16:43:30 +0545

    feat:timeslot availaibility model

--- services/admin.py
+++ services/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Services
+from .models import Services,TimeSlot
 
 @admin.register(Services)
 class ServicesAdmin(admin.ModelAdmin):
     list_display = ['service_name', 'price', 'status', 'created_at']
     list_filter = ['status']
     search_fields = ['service_name']
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display=['start_time','end_time']
+    list_filter=['status']
--- services/models.py
+++ services/models.py
@@ -20,3 +20,16 @@ class Services(models.Model):
 
     def __str__(self):
         return self.service_name
+    
+
+class TimeSlot(models.Model):
+    start_time=models.TimeField()
+    end_time=models.TimeField()
+    status = models.CharField(
+        max_length=20,
+        choices=StatusChoices.choices,
+        default=StatusChoices.ACTIVE)
+
+    def __str__(self):
+        return f"{self.start_time} - {self.end_time}"
+
