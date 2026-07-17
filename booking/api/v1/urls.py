d30383773fbc218f0335ddf2755bc4487b859151
Arun Yogi <arunyogi@Aruns-MacBook-Air.local>
2026-07-17 15:31:42 +0545

    feat:woking on booking update

--- booking/api/v1/urls.py
+++ booking/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BookingView
+from .views import BookingView,BookingManage
 
 urlpatterns=[
-    path("view/",BookingView.as_view(),name="get-post")
+    path("view/",BookingView.as_view(),name="get-post"),
+    path("manage/<int:id>/",BookingManage.as_view(),name="update-delete"),
    
 ]
