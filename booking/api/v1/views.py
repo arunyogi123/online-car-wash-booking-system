f016bef30c402a8531525486e9c63f3acecd85ac
Arun Yogi <yogiarun290@gmail.com>
2026-07-18 19:41:41 +0545

    feat:featuring booking delte

--- booking/api/v1/views.py
+++ booking/api/v1/views.py
@@ -8,57 +8,66 @@ from booking.api.v1.serializers import BookingSerializer
 
 
 class BookingView(GenericAPIView):
-    queryset=Booking.objects.all()
-    serializer_class=BookingSerializer
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
     @extend_schema(
-        responses=BookingSerializer,
-        summary="Booking details view",
-        tags=['booking']
+        responses=BookingSerializer, summary="Booking details view", tags=["booking"]
     )
-    def get(self,request):
-        data=Booking.objects.all()
-        serializer=BookingSerializer(data,many=True)
+    def get(self, request):
+        data = Booking.objects.all()
+        serializer = BookingSerializer(data, many=True)
         return Response(serializer.data)
-    
+
     @extend_schema(
         request=BookingSerializer,
         responses=BookingSerializer,
         summary="create booking",
-        tags=['booking']
+        tags=["booking"],
     )
-    def post(self,request):
-        data=request.data
-        serializer=BookingSerializer(data=data)
+    def post(self, request):
+        data = request.data
+        serializer = BookingSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message":"Booked Successfully","data":serializer.data})
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
+            return Response({"message": "Booked Successfully", "data": serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class BookingManage(GenericAPIView):
-    queryset=Booking.objects.all()
-    serializer_class=BookingSerializer
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
     @extend_schema(
         request=BookingSerializer,
         responses=BookingSerializer,
         summary="Update booking",
-        tags=['booking']
+        tags=["booking"],
     )
-    def put(self,request,id):
-        qs=Booking.objects.get(id=id)
-        data=request.data
-        serializer=BookingSerializer(data=data,instance=qs)
+    def put(self, request, id):
+        qs = Booking.objects.get(id=id, customer=request.user)
+        serializer = BookingSerializer(data=request.data, instance=qs)
         if serializer.is_valid():
             serializer.save()
-            return Response({"message":"Booking Updated Successfully","data":serializer.data},
-                            status=status.HTTP_200_OK)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
+            return Response(
+                {"message": "Booking Updated Successfully", "data": serializer.data},
+                status=status.HTTP_200_OK,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @extend_schema(
+        request=BookingSerializer,
+        responses=BookingSerializer,
+        tags=["booking"]
+
+    )
+    def delete(self,request,id):
+        data=Booking.objects.get(id=id,customer=request.user)
+        data.delete()
+        return Response({"message":"Booking deleted successfully"},status=status.HTTP_200_OK)
 
 
 
     
-    
 
-
-    
