77c4895d1e611a6d7a21d955ea8b9561052e10fa
Arun Yogi <yogiarun290@gmail.com>
2026-08-03 19:17:43 +0545

    feat:featuring opening and closing

---
+++ payment/utils.py
@@ -0,0 +1,14 @@
+# import requests
+
+# class KhaltiPay:
+#     @staticmethod
+#     def get_initial_id(**data):
+#         import json
+
+#         url="-----------Khalti Sandbox-------------"
+#         service_detail=[]
+#         log=data.get("service_log")
+#         for i in log.all():
+#             service_detail.append({
+
+#             })
