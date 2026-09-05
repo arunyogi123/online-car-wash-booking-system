1a6d564e602186d187341a056734fcec61af070e
Arun Yogi <yogiarun290@gmail.com>
2026-09-05 09:25:49 +0545

    feat:booking validation

--- booking/api/v1/serializers.py
+++ booking/api/v1/serializers.py
@@ -5,27 +5,48 @@ from datetime import time
 
 
 class BookingSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Booking
-        fields = "__all__"
+
+        fields = [
+            "id",
+            "customer",
+            "service",
+            "car_model",
+            "car_number_plate",
+            "booking_date",
+            "booking_time",
+            "payment_status",
+            "created_at",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "id",
+            "customer",
+            "payment_status",
+            "created_at",
+            "updated_at",
+        ]
+
         validators = [
             UniqueTogetherValidator(
                 queryset=Booking.objects.all(),
                 fields=["booking_date", "booking_time"],
-                message="This time slot is already booked."
+                message="This time slot is already booked.",
             )
         ]
+
     def validate(self, attrs):
-            booking_time = attrs["booking_time"]
+        booking_time = attrs["booking_time"]
 
-            opening_time = time(6, 0)    
-            closing_time = time(18, 0)  
+        opening_time = time(6, 0)
+        closing_time = time(18, 0)
 
-            if booking_time < opening_time or booking_time > closing_time:
-                raise serializers.ValidationError(
-                    {
-                        "booking_time": "Booking is available only between 6 AM and 6 PM."
-                    }
-                )
+        if booking_time < opening_time or booking_time > closing_time:
+            raise serializers.ValidationError(
+                {"booking_time": "Booking is available only between 6 AM and 6 PM."}
+            )
 
-            return attrs
+        return attrs
